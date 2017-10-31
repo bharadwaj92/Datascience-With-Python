f83672be2dc212b93f063f530b361cfb1297c468
bharadwaj92 <bharadwajt@ymail.com>
2017-10-30 20:47:19 -0500

    added plot

--- mis_6v99_2017_summer-master/Project1/LogisticRegressionNewtomMethod.py
+++ mis_6v99_2017_summer-master/Project1/LogisticRegressionNewtomMethod.py
@@ -7,15 +7,22 @@ binary classification problem, with one training example per row. Implement Newt
 method for optimizing J(θ), and apply it to fit a logistic regression model to the data.
 Initialize Newton’s method with θ = ~0 (the vector of all zeros). What are the coefficients θ
 resulting from your fit? (Remember to include the intercept term.)
+
+[5 points] Plot the training data (your axes should be x1 and x2, corresponding to the two
+coordinates of the inputs, and you should use a different symbol for each point plotted to
+indicate whether that example had label 1 or -1). Also plot on the same figure the decision
+boundary fit by logistic regression. (This should be a straight line showing the boundary
+separating the region where hθ(x) > 0.5 from where hθ(x) ≤ 0.5.)
+
 '''
 from urllib.request import urlopen
 import numpy as np
 from operator import add
-import numdifftools
+import matplotlib.pyplot as plt
 
 
 class LogisticRegression():
-    theta = [5,5,5]
+    theta = [0,0,0]
 
     def sigmoid(self, x, theta):
         z = np.array([(np.dot(x[i], theta[0:x.shape[1]])) for i in range(len(x))])  ## calculates the sigmoid probability 1/(1+ e^-z) where z = theta1 * x1 + theta2*x2 ... + thetan*xn
@@ -33,7 +40,7 @@ class LogisticRegression():
 
     def hessian(self,x, theta):
         sigmoid_probs = self.sigmoid(x, theta)
-        return np.array(np.matmul(np.transpose(x), np.matmul(np.diag(sigmoid_probs * (1 - sigmoid_probs)), x))) ## Calculates second order derivate of cost function : XT*(diag(H(theta)*(1-H(theta))*X 
+        return np.array(np.matmul(np.transpose(x), np.matmul(np.diag(sigmoid_probs * (1 - sigmoid_probs)), x))) ## Calculates second order derivate of cost function : XT*(diag(H(theta)*(1-H(theta))*X
 
     def fit(self, x , y, iterations):
         deltal = np.Infinity
@@ -83,8 +90,17 @@ class DataCleaning():
         y = np.array(yarray)
         return x , y
 
+    def plot(self,x,y):
+        #colors = {'D': 'red', 'E': 'blue', 'F': 'green', 'G': 'black'}
+        plt.scatter(x[:,0],x[:,1] ,c= ['red' if item <0 else 'blue' for item in y])
+
+        plt.show()
+        return
+
 if __name__ == "__main__":
     d = DataCleaning()
     x , y  = d.read_data_files()
     lr = LogisticRegression()
-    lr.fit(x, y , 1)
+    thetavalue = lr.fit(x, y , 0)
+    print(thetavalue)
+    d.plot(x,y)
